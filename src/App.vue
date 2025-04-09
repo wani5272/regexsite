f75f82bdf0e0b995416054c3d267a372aaabfb93
wani5272 <205021230+wani5272@users.noreply.github.com>
2025-04-09 18:27:52 +0200

    add rules 1-3

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,9 @@
         <li v-for="(rule, index) in rules" :key="index">
           <h2>{{ rule.title[currentLanguage] }}</h2>
           <p>{{ rule.description[currentLanguage] }}</p>
-          <code>{{ rule.example }}</code>
+          <p>
+            <code>{{ rule.example }}</code>
+          </p>
         </li>
       </ul>
     </main>
@@ -22,7 +24,19 @@
             ? "Informationen von: "
             : "Informations from: "
         }}
-        <a href="https://man.openbsd.org/re_format.7">Manpage zu Regex</a>
+        <a href="https://regexone.com">regexone.com</a>
+        <a href="https://man.openbsd.org/OpenBSD-7.6/perlre.1">{{
+          currentLanguage === "de"
+            ? "Manpage zu Perl Regex"
+            : "Perl regex manpage"
+        }}</a>
+        <a href="https://man.openbsd.org/OpenBSD-7.6/perlretut.1">
+          {{
+            currentLanguage === "de"
+              ? "Tutorial zu Perl Regex"
+              : "Perl regex tutorial"
+          }}</a
+        >
       </p>
     </footer>
   </div>
@@ -35,12 +49,34 @@ export default {
       currentLanguage: "de",
       rules: [
         {
-          title: { de: "Regel 1", en: "Rule 1" },
+          title: { de: "Wortsuche", en: "Word Matching" },
           description: {
-            de: "Beschreibung für Regel 1",
-            en: "Description for Rule 1",
+            de: 'Nach "Word" suchen',
+            en: 'Search for "Word"',
           },
-          example: "Beispiel für Regel 1",
+          example: "Word",
+        },
+        {
+          title: {
+            de: "Wort am Anfang der Zeile",
+            en: "Match the beginning of a line",
+          },
+          description: {
+            de: 'Nach der Zeile, die mit "Word" beginnt, suchen',
+            en: 'Search for the line beginning with "Word"',
+          },
+          example: "^Word",
+        },
+        {
+          title: {
+            de: "Wort am Ende der Zeile",
+            en: "Match the end of a line",
+          },
+          description: {
+            de: 'Nach der Zeile, die mit "Word" endet, suchen',
+            en: 'Search for the line ending with "Word"',
+          },
+          example: "Word$",
         },
       ],
     };
